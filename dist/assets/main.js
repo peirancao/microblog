f9e6b9f610f2882ca4a9906a848e3147799c878f
peiran <21766691@qq.com>
2016-10-28 20:58:40 +0800

    update

--- dist/assets/main.js
+++ dist/assets/main.js
@@ -12,7 +12,7 @@ Do.add('fastclick', { path: 'assets/fastclick.js?v=1.0.6', type: 'js' });
 Do.add('js.cookie', { path: 'assets/js.cookie.js?v=2.1.2', type: 'js' });
 Do.add('wilddog', { path: 'https://cdn.wilddog.com/js/client/current/wilddog.js', type: 'js' });
 
-Do.add('vue', { path: 'assets/vue.min.js?v=2.0.0-beta.5', type: 'js' });
+Do.add('vue', { path: 'assets/vue.min.js?v=2.0.0-beta.6', type: 'js' });
 
 Do.add('app', { path: 'assets/app.min.js?v=0.0.1', type: 'js', requires: ['vue', 'fastclick', 'EventEmitter', 'timeago', 'wilddog', 'js.cookie', 'director', 'fetch'] });
 
